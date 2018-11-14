15b66d0a6b3acb76199a46717e44e5725d5b714b
wangfei1023 <41721676+wangfei1023@users.noreply.github.com>
2018-11-14 11:09:01 +0800

    Update HelloController.java

--- src/main/java/com/taikang/bd/controller/HelloController.java
+++ src/main/java/com/taikang/bd/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @RequestMapping("/hi")
     public String hello(){
-        return "hello jenkins! wo shi wangfei";
+        return "hello jenkins! wo shi wangfei===================";
     }
 
 }
